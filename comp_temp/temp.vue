c733bfc73b81a7bedce75d02ddf5764c679e17ab
Cloud Deng <cloud.deng@zoom.us>
2017-12-18 10:53:49 +0800

    update vue template

---
+++ comp_temp/temp.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- write template here -->
+
+</template>
+
+{{#if  component.hasTS}}
+<script lang="ts">
+  // write script here
+  import Vue from "vue";
+  export default Vue.extend({
+    name:{{{comonent.name}}},
+    data(){
+      return {
+        
+      }
+    }
+  })
+</script>
+{{else}}
+<script>
+  export default {
+    data(){
+      return {
+         
+      }
+    }
+  }
+</script>
+{{/if}}
+{{#if  package.hasScss}}
+<style lang="scss">
+  // write style here
+
+</style>
+{{else}}
+<style>
+  /* write style here */
+
+</style>
+{{/if}}
